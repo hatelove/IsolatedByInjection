966679fbc841e6794652f051fbf87a6799514a81
91 <jay.hatelove@gmail.com>
2015-02-25 01:09:03 +0800

    Strategy pattern implementation. Get the instance of IStoreService by OrderType

--- SimpleFactoryLegacy/ShipService.cs
+++ SimpleFactoryLegacy/ShipService.cs
@@ -19,45 +19,9 @@ namespace SimpleFactoryLegacy
         Family = 1
     }
 
-    public class ShipService
+    public interface IStoreService
     {
-        public void ShippingByStore(List<Order> orders)
-        {
-            // handle seven's orders
-            var ordersBySeven = orders.Where(x => x.StoreType != StoreType.Family);
-            var sevenService = new SevenService();
-            foreach (var order in ordersBySeven)
-            {
-                sevenService.Ship(order);
-            }
-
-            // handle family's orders
-            var ordersByFamily = orders.Where(x => x.StoreType == StoreType.Family);
-            var familyService = new FamilyService();
-            foreach (var order in ordersByFamily)
-            {
-                familyService.Ship(order);
-            }
-        }
-    }
-
-    public class Order
-    {
-        public StoreType StoreType { get; set; }
-
-        public int Id { get; set; }
-
-        public int Amount { get; set; }
-    }
-
-    public class SevenService : IStoreService
-    {
-        public void Ship(Order order)
-        {
-            // seven web service
-            var client = new HttpClient();
-            client.PostAsync("http://api.seven.com/Order", order, new JsonMediaTypeFormatter());
-        }
+        void Ship(Order order);
     }
 
     public class FamilyService : IStoreService
@@ -70,8 +34,67 @@ namespace SimpleFactoryLegacy
         }
     }
 
-    public interface IStoreService
+    public class Order
     {
-        void Ship(Order order);
+        public int Amount { get; set; }
+
+        public int Id { get; set; }
+
+        public StoreType StoreType { get; set; }
+    }
+
+    public class SevenService : IStoreService
+    {
+        public void Ship(Order order)
+        {
+            // seven web service
+            var client = new HttpClient();
+            client.PostAsync("http://api.seven.com/Order", order, new JsonMediaTypeFormatter());
+        }
+    }
+
+    public class ShipService
+    {
+        private FamilyService _family = new FamilyService();
+
+        private SevenService _seven = new SevenService();
+
+        public void ShippingByStore(List<Order> orders)
+        {
+            //// handle seven's orders
+            //var ordersBySeven = orders.Where(x => x.StoreType != StoreType.Family);
+            //var sevenService = new SevenService();
+            //foreach (var order in ordersBySeven)
+            //{
+            //    sevenService.Ship(order);
+            //}
+
+            //// handle family's orders
+            //var ordersByFamily = orders.Where(x => x.StoreType == StoreType.Family);
+            //var familyService = new FamilyService();
+            //foreach (var order in ordersByFamily)
+            //{
+            //    familyService.Ship(order);
+            //}
+
+            foreach (var order in orders)
+            {
+                // strategy pattern implementation
+                IStoreService storeService = GetStoreService(order);
+                storeService.Ship(order);
+            }
+        }
+
+        private IStoreService GetStoreService(Order order)
+        {
+            if (order.StoreType == StoreType.Family)
+            {
+                return this._family;
+            }
+            else
+            {
+                return this._seven;
+            }
+        }
     }
 }
