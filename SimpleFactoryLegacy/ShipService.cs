f0e40d1339f450f0573cb7af3552141a42dbc1fa
91 <jay.hatelove@gmail.com>
2015-02-25 01:15:02 +0800

    implement simple factory pattern, and split responsibility of creating dependent instance from context to SimpleFactory class

--- SimpleFactoryLegacy/ShipService.cs
+++ SimpleFactoryLegacy/ShipService.cs
@@ -55,45 +55,47 @@ namespace SimpleFactoryLegacy
 
     public class ShipService
     {
-        private FamilyService _family = new FamilyService();
+        //private FamilyService _family = new FamilyService();
 
-        private SevenService _seven = new SevenService();
+        //private SevenService _seven = new SevenService();
 
         public void ShippingByStore(List<Order> orders)
         {
-            //// handle seven's orders
-            //var ordersBySeven = orders.Where(x => x.StoreType != StoreType.Family);
-            //var sevenService = new SevenService();
-            //foreach (var order in ordersBySeven)
-            //{
-            //    sevenService.Ship(order);
-            //}
-
-            //// handle family's orders
-            //var ordersByFamily = orders.Where(x => x.StoreType == StoreType.Family);
-            //var familyService = new FamilyService();
-            //foreach (var order in ordersByFamily)
-            //{
-            //    familyService.Ship(order);
-            //}
-
             foreach (var order in orders)
             {
-                // strategy pattern implementation
-                IStoreService storeService = GetStoreService(order);
+                // simple factory pattern implementation
+                IStoreService storeService = SimpleFactory.GetStoreService(order);
                 storeService.Ship(order);
             }
         }
 
-        private IStoreService GetStoreService(Order order)
+        //private IStoreService GetStoreService(Order order)
+        //{
+        //    if (order.StoreType == StoreType.Family)
+        //    {
+        //        return this._family;
+        //    }
+        //    else
+        //    {
+        //        return this._seven;
+        //    }
+        //}
+    }
+
+    public class SimpleFactory
+    {
+        private static IStoreService sevenService = new SevenService();
+        private static IStoreService familyService = new FamilyService();
+
+        public static IStoreService GetStoreService(Order order)
         {
             if (order.StoreType == StoreType.Family)
             {
-                return this._family;
+                return sevenService;
             }
             else
             {
-                return this._seven;
+                return familyService;
             }
         }
     }
