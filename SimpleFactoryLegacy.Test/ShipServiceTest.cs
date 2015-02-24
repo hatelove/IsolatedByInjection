03d22776bf4b8f738b2c7a3b4575b159f68f9448
91 <jay.hatelove@gmail.com>
2015-02-25 00:59:20 +0800

    initialized sample

---
+++ SimpleFactoryLegacy.Test/ShipServiceTest.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace SimpleFactoryLegacy.Test
+{
+    /// <summary>
+    /// Summary description for ShipServiceTest
+    /// </summary>
+    [TestClass]
+    public class ShipServiceTest
+    {
+        public ShipServiceTest()
+        {
+            //
+            // TODO: Add constructor logic here
+            //
+        }
+
+        private TestContext testContextInstance;
+
+        /// <summary>
+        ///Gets or sets the test context which provides
+        ///information about and functionality for the current test run.
+        ///</summary>
+        public TestContext TestContext
+        {
+            get
+            {
+                return testContextInstance;
+            }
+            set
+            {
+                testContextInstance = value;
+            }
+        }
+
+        #region Additional test attributes
+        //
+        // You can use the following additional attributes as you write your tests:
+        //
+        // Use ClassInitialize to run code before running the first test in the class
+        // [ClassInitialize()]
+        // public static void MyClassInitialize(TestContext testContext) { }
+        //
+        // Use ClassCleanup to run code after all tests in a class have run
+        // [ClassCleanup()]
+        // public static void MyClassCleanup() { }
+        //
+        // Use TestInitialize to run code before running each test 
+        // [TestInitialize()]
+        // public void MyTestInitialize() { }
+        //
+        // Use TestCleanup to run code after each test has run
+        // [TestCleanup()]
+        // public void MyTestCleanup() { }
+        //
+        #endregion
+
+        [TestMethod]
+        public void TestShippingByStore_Seven_1_Order_Family_2_Orders()
+        {
+            //arrange
+            var target = new ShipService();
+
+            var orders = new List<Order>
+            {
+                new Order{ StoreType= StoreType.Seven, Id=1},
+                new Order{ StoreType= StoreType.Family, Id=2},
+                new Order{ StoreType= StoreType.Family, Id=3},
+            };
+
+            //act
+            target.ShippingByStore(orders);
+
+            //todo, assert
+            //ShipService should invoke SevenService once and FamilyService twice
+
+        }
+    }
+}
---
+++ SimpleFactoryLegacy/ShipService.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Formatting;
+using System.Text;
+
+namespace SimpleFactoryLegacy
+{
+    public enum StoreType
+    {
+        /// <summary>
+        /// 7-11
+        /// </summary>
+        Seven = 0,
+
+        /// <summary>
+        /// 全家
+        /// </summary>
+        Family = 1
+    }
+
+    public class ShipService
+    {
+        public void ShippingByStore(List<Order> orders)
+        {
+            // handle seven's orders
+            var ordersBySeven = orders.Where(x => x.StoreType != StoreType.Family);
+            var sevenService = new SevenService();
+            foreach (var order in ordersBySeven)
+            {
+                sevenService.Ship(order);
+            }
+
+            // handle family's orders
+            var ordersByFamily = orders.Where(x => x.StoreType == StoreType.Family);
+            var familyService = new FamilyService();
+            foreach (var order in ordersByFamily)
+            {
+                familyService.Ship(order);
+            }
+        }
+    }
+
+    public class Order
+    {
+        public StoreType StoreType { get; set; }
+        public int Id { get; set; }
+        public int Amount { get; set; }
+    }
+
+    public class SevenService
+    {
+        internal void Ship(Order order)
+        {
+            // seven web service
+            var client = new HttpClient();
+            client.PostAsync("http://api.seven.com/Order", order, new JsonMediaTypeFormatter());
+        }
+    }
+
+    public class FamilyService
+    {
+        internal void Ship(Order order)
+        {
+            // family web service
+            var client = new HttpClient();
+            client.PostAsync("http://api.family.com/Order", order, new JsonMediaTypeFormatter());
+        }
+    }
+}
